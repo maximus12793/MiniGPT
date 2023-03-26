84c3453276cde17b143e8e8bd5205acf3b6695cc
maximus12793 <maximus12793@gmail.com>
2023-03-26 13:13:04 -0500

    decoder complete

--- model_detour/vae.py
+++ model_detour/vae.py
@@ -16,3 +16,38 @@ class TextVAE(nn.Module):
         self.logvar = nn.Linear(hidden_dim, latent_dim)
         self.decoder = nn.LSTMCell(latent_dim, hidden_dim)
         self.output = nn.Linear(hidden_dim, vocab_size)
+
+    def encode(self, x):
+        # Encode the input sequence
+        x = self.embedding(x)
+        x, (hidden, cell) = self.encoder(x)
+        z_mean = self.mean(hidden.squeeze(0))
+        z_logvar = self.logvar(hidden.squeeze(0))
+        return z_mean, z_logvar
+    
+    def reparameterize(self, z_mean, z_logvar):
+        # Reparameterization trick
+        std = torch.exp(0.5 * z_logvar)
+        eps = torch.randn_like(std)
+        z = z_mean + eps * std
+        return z
+
+    def decode(self, z, max_len, device):
+        batch_size = z.size(0)
+        hidden = torch.zeros(batch_size, self.hidden_dim).to(device)
+        cell = torch.zeros(batch_size, self.hidden_dim).to(device)
+
+        outputs = []
+        for i in range(max_len):
+            hidden, cell = self.decoder(z, (hidden, cell))
+            output = self.output(hidden)
+            # Do I need unsqueeze here?
+            outputs.append(output)
+
+            # Use the last output as the next input
+            _, predicted = torch.max(output, 1)
+            input = predicted.unsqueeze(1)
+            z = self.embedding(input)
+
+        outputs = torch.stack(outputs, dim=1)
+        return outputs
